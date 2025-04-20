fd91ed7de59290a4deac79fdb1127e9b39646fc1
wuweichi <wuduqi@gmail.com>
2025-04-21 03:10:15 +0800

    feat: 添加生成的代码 daily_codes/complex_program/main.go
    生成时间: 2025-04-20 19:10:14

--- daily_codes/complex_program/main.go
+++ daily_codes/complex_program/main.go
@@ -8,15 +8,15 @@ import (
 
 func generateRandomString(length int) string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+		b[i] = charset[rand.Intn(len(charset))]
 	}
 	return string(b)
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < 1000; i++ {
 		randomString := generateRandomString(10)
 		fmt.Printf("Random string %d: %s\n", i+1, randomString)
