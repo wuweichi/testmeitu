4a9b41f1521cb0077b26d007117f6c3e77e5d1fb
wuweichi <wuduqi@gmail.com>
2025-04-24 02:10:17 +0800

    feat: 添加生成的代码 daily_codes/complex_go_program/main.go
    生成时间: 2025-04-23 18:10:15

--- daily_codes/complex_go_program/main.go
+++ daily_codes/complex_go_program/main.go
@@ -6,48 +6,16 @@ import (
 	"time"
 )
 
-// ComplexStruct represents a complex data structure
-type ComplexStruct struct {
-	ID      int
-	Name    string
-	Details []Detail
-}
-
-// Detail represents additional details
-type Detail struct {
-	Key   string
-	Value string
-}
-
-func generateRandomComplexStruct() ComplexStruct {
+func generateRandomNumber() int {
 	rand.Seed(time.Now().UnixNano())
-	details := make([]Detail, rand.Intn(5)+1)
-	for i := range details {
-		details[i] = Detail{
-			Key:   fmt.Sprintf("Key%d", i),
-			Value: fmt.Sprintf("Value%d", rand.Intn(100)),
-		}
-	}
-	return ComplexStruct{
-		ID:      rand.Intn(1000),
-		Name:    fmt.Sprintf("Name%d", rand.Intn(100)),
-		Details: details,
-	}
-}
-
-func printComplexStruct(cs ComplexStruct) {
-	fmt.Printf("ID: %d\n", cs.ID)
-	fmt.Printf("Name: %s\n", cs.Name)
-	fmt.Println("Details:")
-	for _, detail := range cs.Details {
-		fmt.Printf("\t%s: %s\n", detail.Key, detail.Value)
-	}
+	return rand.Intn(100)
 }
 
 func main() {
-	for i := 0; i < 100; i++ {
-		cs := generateRandomComplexStruct()
-		printComplexStruct(cs)
-		fmt.Println("-----------------")
+	fmt.Println("Starting the complex Go program...")
+	for i := 0; i < 1000; i++ {
+		num := generateRandomNumber()
+		fmt.Printf("Generated random number %d: %d\n", i+1, num)
 	}
+	fmt.Println("Program completed successfully.")
 }
