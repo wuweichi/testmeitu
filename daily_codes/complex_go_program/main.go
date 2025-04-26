e2ea401d4e665cfcb0db1fde595ce3e281393dc3
wuweichi <wuduqi@gmail.com>
2025-04-27 03:10:28 +0800

    feat: 添加生成的代码 daily_codes/complex_go_program/main.go
    生成时间: 2025-04-26 19:10:27

--- daily_codes/complex_go_program/main.go
+++ daily_codes/complex_go_program/main.go
@@ -6,14 +6,67 @@ import (
 	"time"
 )
 
-func generateRandomNumber(min, max int) int {
-	return rand.Intn(max-min) + min
+// ComplexStruct represents a complex data structure
+type ComplexStruct struct {
+	ID       int
+	Name     string
+	Details  []string
+	Metadata map[string]interface{}
 }
 
+// generateRandomString generates a random string of given length
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+// populateComplexStruct creates and populates a ComplexStruct with random data
+func populateComplexStruct() ComplexStruct {
+	cs := ComplexStruct{
+		ID:   rand.Intn(1000),
+		Name: generateRandomString(10),
+	}
+
+	detailsCount := rand.Intn(5) + 1
+	for i := 0; i < detailsCount; i++ {
+		cs.Details = append(cs.Details, generateRandomString(20))
+	}
+
+	cs.Metadata = make(map[string]interface{})
+	metadataCount := rand.Intn(3) + 1
+	for i := 0; i < metadataCount; i++ {
+		cs.Metadata[generateRandomString(5)] = rand.Intn(100)
+	}
+
+	return cs
+}
+
+// main is the entry point of the program
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 1000; i++ {
-		randomNum := generateRandomNumber(1, 100)
-		fmt.Printf("Random number %d: %d\n", i+1, randomNum)
+
+	// Generate a slice of ComplexStruct
+	var complexStructs []ComplexStruct
+	for i := 0; i < 100; i++ {
+		complexStructs = append(complexStructs, populateComplexStruct())
+	}
+
+	// Print each ComplexStruct
+	for _, cs := range complexStructs {
+		fmt.Printf("ID: %d, Name: %s\n", cs.ID, cs.Name)
+		fmt.Println("Details:")
+		for _, detail := range cs.Details {
+			fmt.Printf("\t%s\n", detail)
+		}
+		fmt.Println("Metadata:")
+		for key, value := range cs.Metadata {
+			fmt.Printf("\t%s: %v\n", key, value)
+		}
+		fmt.Println()
 	}
 }
