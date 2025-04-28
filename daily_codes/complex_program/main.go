65d70132c9f5be30e6f592dd5f63116c64e8401c
wuweichi <wuduqi@gmail.com>
2025-04-29 02:10:15 +0800

    feat: 添加生成的代码 daily_codes/complex_program/main.go
    生成时间: 2025-04-28 18:10:14

--- daily_codes/complex_program/main.go
+++ daily_codes/complex_program/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func generateRandomNumber(min, max int) int {
-	return min + rand.Intn(max-min)
+	return rand.Intn(max-min) + min
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < 1000; i++ {
-		randomNumber := generateRandomNumber(1, 100)
-		fmt.Printf("Random number %d: %d\n", i+1, randomNumber)
+		randomNum := generateRandomNumber(1, 100)
+		fmt.Printf("Random number %d: %d\n", i+1, randomNum)
 	}
 }
