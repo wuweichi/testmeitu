df39230690c7053741f77ad9780093df9b1371f6
wuweichi <wuduqi@gmail.com>
2025-05-13 02:10:15 +0800

    feat: 添加生成的代码 daily_codes/funny_go_program/main.go
    生成时间: 2025-05-12 18:10:14

--- daily_codes/funny_go_program/main.go
+++ daily_codes/funny_go_program/main.go
@@ -8,23 +8,12 @@ import (
 
 func main() {
 	fmt.Println("Welcome to the Funny Number Generator!")
-	fmt.Println("Generating a sequence of funny numbers...")
+	fmt.Println("Generating 1000 random numbers between 1 and 100...")
 	
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < 1000; i++ {
-		funnyNumber := rand.Intn(100) + 1
-		fmt.Printf("Funny number %d: %d\n", i+1, funnyNumber)
-		
-		switch {
-		case funnyNumber < 20:
-			fmt.Println("That's a tiny number!")
-		case funnyNumber < 50:
-			fmt.Println("That's a medium number!")
-		default:
-			fmt.Println("That's a big number!")
-		}
-		
-		time.Sleep(100 * time.Millisecond)
+		randomNumber := rand.Intn(100) + 1
+		fmt.Printf("Number %d: %d\n", i+1, randomNumber)
 	}
-	fmt.Println("Finished generating funny numbers!")
+	fmt.Println("Done generating numbers. Hope you had fun!")
 }
