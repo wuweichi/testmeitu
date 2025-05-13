aab4139a4b772f97fbbd4527112fa4c4f312d049
wuweichi <wuduqi@gmail.com>
2025-05-14 03:10:17 +0800

    feat: 添加生成的代码 daily_codes/funny_go_program/main.go
    生成时间: 2025-05-13 19:10:16

--- daily_codes/funny_go_program/main.go
+++ daily_codes/funny_go_program/main.go
@@ -7,13 +7,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to the Funny Number Generator!")
-	fmt.Println("Generating 1000 random numbers between 1 and 100...")
-	
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < 1000; i++ {
-		randomNumber := rand.Intn(100) + 1
-		fmt.Printf("Number %d: %d\n", i+1, randomNumber)
+		switch rand.Intn(5) {
+		case 0:
+			fmt.Println("Why don't scientists trust atoms? Because they make up everything!")
+		case 1:
+			fmt.Println("Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them.")
+		case 2:
+			fmt.Println("Why did the scarecrow win an award? Because he was outstanding in his field!")
+		case 3:
+			fmt.Println("I told my wife she was drawing her eyebrows too high. She looked surprised.")
+		case 4:
+			fmt.Println("Why can't you explain puns to kleptomaniacs? Because they always take things literally.")
+		}
 	}
-	fmt.Println("Done generating numbers. Hope you had fun!")
 }
