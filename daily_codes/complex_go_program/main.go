2cfa186431f955151e784524ef1abd563e6bbc8c
wuweichi <wuduqi@gmail.com>
2025-05-18 03:10:23 +0800

    feat: 添加生成的代码 daily_codes/complex_go_program/main.go
    生成时间: 2025-05-17 19:10:22

--- daily_codes/complex_go_program/main.go
+++ daily_codes/complex_go_program/main.go
@@ -6,19 +6,45 @@ import (
 	"time"
 )
 
-func generateRandomString(length int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+// ComplexStruct represents a complex data structure
+type ComplexStruct struct {
+	ID          int
+	Name        string
+	Description string
+	Values      []int
+	Timestamp   time.Time
+}
+
+// generateRandomComplexStruct generates a ComplexStruct with random values
+func generateRandomComplexStruct() ComplexStruct {
+	rand.Seed(time.Now().UnixNano())
+	values := make([]int, rand.Intn(10)+5)
+	for i := range values {
+		values[i] = rand.Intn(100)
 	}
-	return string(b)
+	return ComplexStruct{
+		ID:          rand.Intn(1000),
+		Name:        fmt.Sprintf("Name%d", rand.Intn(100)),
+		Description: fmt.Sprintf("Description%d", rand.Intn(100)),
+		Values:      values,
+		Timestamp:   time.Now(),
+	}
+}
+
+// printComplexStruct prints the details of a ComplexStruct
+func printComplexStruct(cs ComplexStruct) {
+	fmt.Printf("ID: %d\n", cs.ID)
+	fmt.Printf("Name: %s\n", cs.Name)
+	fmt.Printf("Description: %s\n", cs.Description)
+	fmt.Printf("Values: %v\n", cs.Values)
+	fmt.Printf("Timestamp: %s\n", cs.Timestamp.Format(time.RFC3339))
 }
 
 func main() {
-	for i := 0; i < 1000; i++ {
-		randomString := generateRandomString(10)
-		fmt.Printf("Random string %d: %s\n", i+1, randomString)
+	// Generate and print 100 random ComplexStructs
+	for i := 0; i < 100; i++ {
+		cs := generateRandomComplexStruct()
+		printComplexStruct(cs)
+		fmt.Println("----------------------------------------")
 	}
 }
