e4e07f6f5d43d6435032613c0f05c138df5597b1
wuweichi <wuduqi@gmail.com>
2025-05-19 03:10:16 +0800

    feat: 添加生成的代码 daily_codes/complex_program/main.go
    生成时间: 2025-05-18 19:10:15

--- daily_codes/complex_program/main.go
+++ daily_codes/complex_program/main.go
@@ -6,16 +6,19 @@ import (
 	"time"
 )
 
-func generateRandomNumber(min, max int) int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(max-min+1) + min
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
 
 func main() {
-	fmt.Println("Welcome to the Complex Number Generator!")
 	for i := 0; i < 1000; i++ {
-		randomNum := generateRandomNumber(1, 1000)
-		fmt.Printf("Generated random number %d: %d\n", i+1, randomNum)
+		randomString := generateRandomString(10)
+		fmt.Printf("Random string %d: %s\n", i+1, randomString)
 	}
-	fmt.Println("Finished generating numbers!")
 }
