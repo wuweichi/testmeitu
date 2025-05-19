da0e3d3712508712e936d7343a0fec19f6474bc5
wuweichi <wuduqi@gmail.com>
2025-05-20 03:10:23 +0800

    feat: 添加生成的代码 daily_codes/fun_go_program/main.go
    生成时间: 2025-05-19 19:10:22

--- daily_codes/fun_go_program/main.go
+++ daily_codes/fun_go_program/main.go
@@ -7,26 +7,32 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to the Fun Go Program!")
-	fmt.Println("Generating a random number between 1 and 100...")
+	// Seed the random number generator
 	rand.Seed(time.Now().UnixNano())
-	randomNumber := rand.Intn(100) + 1
-	fmt.Printf("The random number is: %d\n", randomNumber)
-	fmt.Println("Let's play a guessing game! Try to guess the number.")
+
+	// Generate a random number between 1 and 100
+	target := rand.Intn(100) + 1
+	fmt.Println("I've picked a number between 1 and 100. Can you guess it?")
+
 	var guess int
 	attempts := 0
+
 	for {
 		fmt.Print("Enter your guess: ")
-		fmt.Scan(&guess)
+		_, err := fmt.Scanf("%d", &guess)
+		if err != nil {
+			fmt.Println("Please enter a valid number.")
+			continue
+		}
 		attempts++
-		if guess < randomNumber {
+
+		if guess < target {
 			fmt.Println("Too low! Try again.")
-		} else if guess > randomNumber {
+		} else if guess > target {
 			fmt.Println("Too high! Try again.")
 		} else {
-			fmt.Printf("Congratulations! You guessed the number in %d attempts.\n", attempts)
+			fmt.Printf("Congratulations! You've guessed the number in %d attempts.\n", attempts)
 			break
 		}
 	}
-	fmt.Println("Thanks for playing the Fun Go Program!")
 }
