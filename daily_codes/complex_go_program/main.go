99e4c53a75a0950d9bc332efd28aca6e1a3cff47
wuweichi <wuduqi@gmail.com>
2025-05-21 03:10:15 +0800

    feat: 添加生成的代码 daily_codes/complex_go_program/main.go
    生成时间: 2025-05-20 19:10:14

--- daily_codes/complex_go_program/main.go
+++ daily_codes/complex_go_program/main.go
@@ -6,45 +6,19 @@ import (
 	"time"
 )
 
-// ComplexStruct represents a complex data structure
-type ComplexStruct struct {
-	ID          int
-	Name        string
-	Description string
-	Values      []int
-	Timestamp   time.Time
-}
-
-// generateRandomComplexStruct generates a ComplexStruct with random values
-func generateRandomComplexStruct() ComplexStruct {
-	rand.Seed(time.Now().UnixNano())
-	values := make([]int, rand.Intn(10)+5)
-	for i := range values {
-		values[i] = rand.Intn(100)
-	}
-	return ComplexStruct{
-		ID:          rand.Intn(1000),
-		Name:        fmt.Sprintf("Name%d", rand.Intn(100)),
-		Description: fmt.Sprintf("Description%d", rand.Intn(100)),
-		Values:      values,
-		Timestamp:   time.Now(),
-	}
-}
-
-// printComplexStruct prints the details of a ComplexStruct
-func printComplexStruct(cs ComplexStruct) {
-	fmt.Printf("ID: %d\n", cs.ID)
-	fmt.Printf("Name: %s\n", cs.Name)
-	fmt.Printf("Description: %s\n", cs.Description)
-	fmt.Printf("Values: %v\n", cs.Values)
-	fmt.Printf("Timestamp: %s\n", cs.Timestamp.Format(time.RFC3339))
+func generateRandomNumber(min, max int) int {
+	return rand.Intn(max-min) + min
 }
 
 func main() {
-	// Generate and print 100 random ComplexStructs
-	for i := 0; i < 100; i++ {
-		cs := generateRandomComplexStruct()
-		printComplexStruct(cs)
-		fmt.Println("----------------------------------------")
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("Generating 1000 random numbers between 1 and 100:")
+	for i := 0; i < 1000; i++ {
+		randomNumber := generateRandomNumber(1, 100)
+		fmt.Printf("%d ", randomNumber)
+		if (i+1)%10 == 0 {
+			fmt.Println()
+		}
 	}
+	fmt.Println("\nDone generating random numbers.")
 }
