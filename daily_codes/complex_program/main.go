65ea6cb22a450a54f2047840e699f74b7bff63f7
wuweichi <wuduqi@gmail.com>
2025-06-05 02:10:32 +0800

    feat: 添加生成的代码 daily_codes/complex_program/main.go
    生成时间: 2025-06-04 18:10:31

--- daily_codes/complex_program/main.go
+++ daily_codes/complex_program/main.go
@@ -6,18 +6,60 @@ import (
 	"time"
 )
 
-func generateRandomString(length int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+// ComplexStruct represents a complex data structure
+type ComplexStruct struct {
+	ID      int
+	Name    string
+	Details []Detail
+}
+
+// Detail represents details of the ComplexStruct
+type Detail struct {
+	Field1 string
+	Field2 int
+	Field3 bool
+}
+
+func generateRandomDetail() Detail {
+	rand.Seed(time.Now().UnixNano())
+	return Detail{
+		Field1: fmt.Sprintf("Field1-%d", rand.Intn(100)),
+		Field2: rand.Intn(1000),
+		Field3: rand.Intn(2) == 1,
 	}
-	return string(b)
+}
+
+func generateComplexStruct(id int, name string, detailCount int) ComplexStruct {
+	details := make([]Detail, detailCount)
+	for i := range details {
+		details[i] = generateRandomDetail()
+	}
+	return ComplexStruct{
+		ID:      id,
+		Name:    name,
+		Details: details,
+	}
+}
+
+func printComplexStruct(cs ComplexStruct) {
+	fmt.Printf("ID: %d, Name: %s\n", cs.ID, cs.Name)
+	for i, detail := range cs.Details {
+		fmt.Printf("\tDetail %d: Field1=%s, Field2=%d, Field3=%t\n", i+1, detail.Field1, detail.Field2, detail.Field3)
+	}
 }
 
 func main() {
-	for i := 0; i < 1000; i++ {
-		fmt.Println(generateRandomString(10))
+	// Generate a slice of ComplexStruct
+	var complexStructs []ComplexStruct
+	for i := 1; i <= 100; i++ {
+		cs := generateComplexStruct(i, fmt.Sprintf("Name-%d", i), 10)
+		complexStructs = append(complexStructs, cs)
 	}
+
+	// Print all ComplexStruct
+	for _, cs := range complexStructs {
+		printComplexStruct(cs)
+	}
+
+	fmt.Println("Program completed successfully.")
 }
