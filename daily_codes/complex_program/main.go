2378cb68acb18b185b0460d4199c42c5afa0eb22
wuweichi <wuduqi@gmail.com>
2025-06-08 03:10:14 +0800

    feat: 添加生成的代码 daily_codes/complex_program/main.go
    生成时间: 2025-06-07 19:10:13

--- daily_codes/complex_program/main.go
+++ daily_codes/complex_program/main.go
@@ -6,18 +6,22 @@ import (
 	"time"
 )
 
-func generateRandomString(length int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
-	}
-	return string(b)
+func generateRandomNumber() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(100)
 }
 
 func main() {
+	fmt.Println("Starting complex program...")
 	for i := 0; i < 1000; i++ {
-		fmt.Println(generateRandomString(10))
+		number := generateRandomNumber()
+		fmt.Printf("Generated random number %d: %d\n", i+1, number)
+		if number%2 == 0 {
+			fmt.Println("The number is even.")
+		} else {
+			fmt.Println("The number is odd.")
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
+	fmt.Println("Program finished.")
 }
