90f55da29f6b54cc09d3c3ef4274397851f2b41e
wuweichi <wuduqi@gmail.com>
2025-06-11 02:10:18 +0800

    feat: 添加生成的代码 daily_codes/complex_go_program/main.go
    生成时间: 2025-06-10 18:10:17

--- daily_codes/complex_go_program/main.go
+++ daily_codes/complex_go_program/main.go
@@ -6,20 +6,15 @@ import (
 	"time"
 )
 
-func generateRandomNumber() int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(100)
+func generateRandomNumber(min, max int) int {
+	return rand.Intn(max-min) + min
 }
 
 func main() {
-	fmt.Println("Welcome to the Complex Go Program!")
-	fmt.Println("Generating a random number between 0 and 99...")
-	randomNumber := generateRandomNumber()
-	fmt.Printf("Your random number is: %d\n", randomNumber)
-	
+	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < 1000; i++ {
-		fmt.Printf("Loop iteration %d: Still running...\n", i)
+		randomNumber := generateRandomNumber(1, 100)
+		fmt.Printf("Random number %d: %d\n", i+1, randomNumber)
 	}
-	
-	fmt.Println("Program completed successfully!")
+	fmt.Println("Generated 1000 random numbers between 1 and 100.")
 }
