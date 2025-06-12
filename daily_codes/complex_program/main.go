4b55818408735d992a0d66c0203494c3b453bcb4
wuweichi <wuduqi@gmail.com>
2025-06-13 02:10:18 +0800

    feat: 添加生成的代码 daily_codes/complex_program/main.go
    生成时间: 2025-06-12 18:10:17

--- daily_codes/complex_program/main.go
+++ daily_codes/complex_program/main.go
@@ -4,36 +4,21 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
-	"sync"
 )
 
-func generateRandomNumbers(count int, wg *sync.WaitGroup, ch chan int) {
-	defer wg.Done()
-	rand.Seed(time.Now().UnixNano()
-	for i := 0; i < count; i++ {
-		ch <- rand.Intn(100)
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
 	}
-}
-
-func processNumbers(ch chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for num := range ch {
-		fmt.Printf("Processed number: %d\n", num*2)
-	}
+	return string(b)
 }
 
 func main() {
-	const numCount = 1000
-	var wg sync.WaitGroup
-	ch := make(chan int, numCount)
-
-	wg.Add(1)
-	go generateRandomNumbers(numCount, &wg, ch)
-
-	wg.Add(1)
-	go processNumbers(ch, &wg)
-
-	wg.Wait()
-	close(ch)
-	fmt.Println("All numbers processed.")
+	for i := 0; i < 1000; i++ {
+		randomString := generateRandomString(10)
+		fmt.Printf("Random String %d: %s\n", i+1, randomString)
+	}
 }
