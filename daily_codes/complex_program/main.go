b3039531f76f06e8ff374b1794871a06be918d6e
wuweichi <wuduqi@gmail.com>
2025-06-14 03:10:15 +0800

    feat: 添加生成的代码 daily_codes/complex_program/main.go
    生成时间: 2025-06-13 19:10:14

--- daily_codes/complex_program/main.go
+++ daily_codes/complex_program/main.go
@@ -6,39 +6,18 @@ import (
 	"time"
 )
 
-func generateRandomNumber(min, max int) int {
-	rand.Seed(time.Now().UnixNano()
-	return rand.Intn(max-min+1) + min
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
 
 func main() {
-	fmt.Println("Welcome to the Complex Program!")
-	fmt.Println("Generating a list of random numbers...")
-	
-	var numbers []int
 	for i := 0; i < 1000; i++ {
-		numbers = append(numbers, generateRandomNumber(1, 1000))
+		fmt.Println(generateRandomString(10))
 	}
-	
-	fmt.Println("Numbers generated:", numbers)
-	
-	fmt.Println("Calculating the sum of all numbers...")
-	sum := 0
-	for _, num := range numbers {
-		sum += num
-	}
-	
-	fmt.Println("Total sum:", sum)
-	
-	fmt.Println("Finding the largest number...")
-	largest := numbers[0]
-	for _, num := range numbers {
-		if num > largest {
-			largest = num
-		}
-	}
-	
-	fmt.Println("Largest number:", largest)
-	
-	fmt.Println("Program finished.")
 }
