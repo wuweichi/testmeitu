58631f110edacfbca653ef1697115093f49ee948
wuweichi <wuduqi@gmail.com>
2025-06-25 02:10:19 +0800

    feat: 添加生成的代码 daily_codes/fun_go_program/main.go
    生成时间: 2025-06-24 18:10:18

--- daily_codes/fun_go_program/main.go
+++ daily_codes/fun_go_program/main.go
@@ -7,33 +7,33 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to the Fun Go Program!")
-	fmt.Println("This program generates random numbers and checks if they're prime.")
-	fmt.Println("Let's get started!")
-
 	// Seed the random number generator
 	rand.Seed(time.Now().UnixNano())
 
-	// Generate and check 1000 random numbers
-	for i := 0; i < 1000; i++ {
-		n := rand.Intn(10000) + 1
-		if isPrime(n) {
-			fmt.Printf("%d is a prime number.\n", n)
-		} else {
-			fmt.Printf("%d is not a prime number.\n", n)
-		}
-	}
-}
+	// Generate a random number between 1 and 100
+	target := rand.Intn(100) + 1
 
-// isPrime checks if a number is prime
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 {
-			return false
+	fmt.Println("I've picked a random number between 1 and 100. Can you guess it?")
+
+	var guess int
+	attempts := 0
+
+	for {
+		fmt.Print("Enter your guess: ")
+		_, err := fmt.Scanf("%d", &guess)
+		if err != nil {
+			fmt.Println("Please enter a valid number.")
+			continue
+		}
+		attempts++
+
+		if guess < target {
+			fmt.Println("Too low! Try again.")
+		} else if guess > target {
+			fmt.Println("Too high! Try again.")
+		} else {
+			fmt.Printf("Congratulations! You guessed the number in %d attempts.\n", attempts)
+			break
 		}
 	}
-	return true
 }
