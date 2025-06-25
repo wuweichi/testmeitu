83fe73d0a12cf334d9d698e6d4b2802980b3a379
wuweichi <wuduqi@gmail.com>
2025-06-26 03:10:21 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-06-25 19:10:20

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -11,15 +11,16 @@ var quotes = []string{
 	"Life is what happens when you're busy making other plans. - John Lennon",
 	"The way to get started is to quit talking and begin doing. - Walt Disney",
 	"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
-	"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. - Oprah Winfrey",
-	"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. - Helen Keller",
-	"Keep your face always toward the sunshine - and shadows will fall behind you. - Walt Whitman",
-	"You will face many defeats in life, but never let yourself be defeated. - Maya Angelou",
-	"The only impossible journey is the one you never begin. - Tony Robbins",
-	"In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
+	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
+	"You must be the change you wish to see in the world. - Mahatma Gandhi",
+	"Whatever you are, be a good one. - Abraham Lincoln",
+	"The only way to do great work is to love what you do. - Steve Jobs",
+	"If you can dream it, you can do it. - Walt Disney",
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println(quotes[rand.Intn(len(quotes))])
+	randomQuote := quotes[rand.Intn(len(quotes))]
+	fmt.Println(randomQuote)
 }
