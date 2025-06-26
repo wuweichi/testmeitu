9fb05edb3da35c7757ebaf251afe1a2a5db5f4d2
wuweichi <wuduqi@gmail.com>
2025-06-27 02:10:24 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-06-26 18:10:23

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -6,21 +6,21 @@ import (
 	"time"
 )
 
-var quotes = []string{
-	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-	"Life is what happens when you're busy making other plans. - John Lennon",
-	"The way to get started is to quit talking and begin doing. - Walt Disney",
-	"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
-	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-	"You must be the change you wish to see in the world. - Mahatma Gandhi",
-	"Whatever you are, be a good one. - Abraham Lincoln",
-	"The only way to do great work is to love what you do. - Steve Jobs",
-	"If you can dream it, you can do it. - Walt Disney",
-}
-
 func main() {
+	quotes := []string{
+		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+		"Life is what happens when you're busy making other plans. - John Lennon",
+		"The way to get started is to quit talking and begin doing. - Walt Disney",
+		"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+		"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
+		"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
+		"The only way to do great work is to love what you do. - Steve Jobs",
+		"If you can dream it, you can do it. - Walt Disney",
+		"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+		"Believe you can and you're halfway there. - Theodore Roosevelt",
+	}
+
 	rand.Seed(time.Now().UnixNano())
-	randomQuote := quotes[rand.Intn(len(quotes))]
-	fmt.Println(randomQuote)
+	randomIndex := rand.Intn(len(quotes))
+	fmt.Println(quotes[randomIndex])
 }
