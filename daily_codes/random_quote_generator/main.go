5e83676db5e6a4e8c73d4f45705782deadc9e9a9
wuweichi <wuduqi@gmail.com>
2025-06-28 03:10:21 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-06-27 19:10:20

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -6,21 +6,18 @@ import (
 	"time"
 )
 
-func main() {
-	quotes := []string{
-		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-		"Life is what happens when you're busy making other plans. - John Lennon",
-		"You must be the change you wish to see in the world. - Mahatma Gandhi",
-		"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-		"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment. - Buddha",
-		"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-		"In the middle of difficulty lies opportunity. - Albert Einstein",
-		"The only way to do great work is to love what you do. - Steve Jobs",
-		"It is never too late to be what you might have been. - George Eliot",
-		"Happiness is not something ready made. It comes from your own actions. - Dalai Lama",
-	}
+var quotes = []string{
+	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+	"Life is what happens when you're busy making other plans. - John Lennon",
+	"The way to get started is to quit talking and begin doing. - Walt Disney",
+	"Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+	"If life were predictable it would cease to be life, and be without flavor. - Eleanor Roosevelt",
+	"If you look at what you have in life, you'll always have more. - Oprah Winfrey",
+	"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. - James Cameron",
+	"You may say I'm a dreamer, but I'm not the only one. - John Lennon",
+}
 
+func main() {
 	rand.Seed(time.Now().UnixNano())
-	randomIndex := rand.Intn(len(quotes))
-	fmt.Println(quotes[randomIndex])
+	fmt.Println(quotes[rand.Intn(len(quotes))])
 }
