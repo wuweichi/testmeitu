3908052d9119b575b48d052d5fcfb5fcb1ba8e51
wuweichi <wuduqi@gmail.com>
2025-06-28 02:10:22 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-06-27 18:10:21

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -10,14 +10,14 @@ func main() {
 	quotes := []string{
 		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 		"Life is what happens when you're busy making other plans. - John Lennon",
-		"The way to get started is to quit talking and begin doing. - Walt Disney",
+		"You must be the change you wish to see in the world. - Mahatma Gandhi",
 		"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-		"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
-		"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-		"The only way to do great work is to love what you do. - Steve Jobs",
-		"If you can dream it, you can do it. - Walt Disney",
+		"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment. - Buddha",
 		"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-		"Believe you can and you're halfway there. - Theodore Roosevelt",
+		"In the middle of difficulty lies opportunity. - Albert Einstein",
+		"The only way to do great work is to love what you do. - Steve Jobs",
+		"It is never too late to be what you might have been. - George Eliot",
+		"Happiness is not something ready made. It comes from your own actions. - Dalai Lama",
 	}
 
 	rand.Seed(time.Now().UnixNano())
