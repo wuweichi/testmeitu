a304b554c99ddbdd235c98acbae9632340aaf816
wuweichi <wuduqi@gmail.com>
2025-07-01 03:10:22 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-06-30 19:10:21

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -10,11 +10,13 @@ var quotes = []string{
 	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 	"Life is what happens when you're busy making other plans. - John Lennon",
 	"The way to get started is to quit talking and begin doing. - Walt Disney",
-	"Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
-	"If life were predictable it would cease to be life, and be without flavor. - Eleanor Roosevelt",
-	"If you look at what you have in life, you'll always have more. - Oprah Winfrey",
-	"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. - James Cameron",
-	"You may say I'm a dreamer, but I'm not the only one. - John Lennon",
+	"Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
+	"You must be the change you wish to see in the world. - Mahatma Gandhi",
+	"Whatever you are, be a good one. - Abraham Lincoln",
+	"The best way to predict the future is to invent it. - Alan Kay",
+	"Strive not to be a success, but rather to be of value. - Albert Einstein",
 }
 
 func main() {
