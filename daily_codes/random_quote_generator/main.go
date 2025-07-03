d385cc1d017c6b4d6977e6d8663da037c70fb494
wuweichi <wuduqi@gmail.com>
2025-07-04 03:10:18 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-07-03 19:10:17

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -7,19 +7,16 @@ import (
 )
 
 var quotes = []string{
-	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+	"The only way to do great work is to love what you do. - Steve Jobs",
 	"Life is what happens when you're busy making other plans. - John Lennon",
-	"The way to get started is to quit talking and begin doing. - Walt Disney",
-	"Your time is limited, don't waste it living someone else's life. - Steve Jobs",
 	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-	"You must be the change you wish to see in the world. - Mahatma Gandhi",
-	"Whatever you are, be a good one. - Abraham Lincoln",
-	"The best way to predict the future is to invent it. - Alan Kay",
-	"Strive not to be a success, but rather to be of value. - Albert Einstein",
+	"It is during our darkest moments that we must focus to see the light. - Aristotle",
+	"Whoever is happy will make others happy too. - Anne Frank",
+	"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println(quotes[rand.Intn(len(quotes))])
+	quote := quotes[rand.Intn(len(quotes))]
+	fmt.Println(quote)
 }
