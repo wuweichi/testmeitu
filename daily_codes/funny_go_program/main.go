6bd3674a4ea2c473de453e0a107aa78c144e5974
wuweichi <wuduqi@gmail.com>
2025-07-05 02:10:12 +0800

    feat: 添加生成的代码 daily_codes/funny_go_program/main.go
    生成时间: 2025-07-04 18:10:11

--- daily_codes/funny_go_program/main.go
+++ daily_codes/funny_go_program/main.go
@@ -7,48 +7,13 @@ import (
 )
 
 func main() {
-	// Seed the random number generator
+	fmt.Println("Welcome to the Funny Number Generator!")
+	fmt.Println("Generating 1000 random numbers between 1 and 100...")
+	
 	rand.Seed(time.Now().UnixNano())
-
-	// A funny story generator
-	stories := []string{
-		"Why don't scientists trust atoms? Because they make up everything!",
-		"Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them.",
-		"Why don't skeletons fight each other? They don't have the guts.",
-		"I told my wife she was drawing her eyebrows too high. She looked surprised.",
-		"What do you call a fake noodle? An impasta!",
-	}
-
-	// Generate a random index
-	index := rand.Intn(len(stories))
-
-	// Print a random funny story
-	fmt.Println(stories[index])
-
-	// A simple number guessing game
-	fmt.Println("Let's play a game! Guess the number between 1 and 10.")
-	target := rand.Intn(10) + 1
-
-	for {
-		var guess int
-		fmt.Print("Enter your guess: ")
-		fmt.Scan(&guess)
-
-		if guess == target {
-			fmt.Println("Congratulations! You guessed it right.")
-			break
-		} else if guess < target {
-			fmt.Println("Too low! Try again.")
-		} else {
-			fmt.Println("Too high! Try again.")
-		}
-	}
-
-	// The program continues with more functionalities to meet the line requirement
-	// This is a placeholder for additional code to reach over 1000 lines
-	// In practice, you would add more meaningful and complex functionalities
 	for i := 0; i < 1000; i++ {
-		// Dummy loop to simulate a longer program
-		_ = i
+		randomNumber := rand.Intn(100) + 1
+		fmt.Printf("Number %d: %d\n", i+1, randomNumber)
 	}
+	fmt.Println("Done generating numbers. Have a funny day!")
 }
