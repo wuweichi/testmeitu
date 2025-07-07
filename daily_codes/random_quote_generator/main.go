c3839193b09940b678cd4acf2b36c41d1fee90ea
wuweichi <wuduqi@gmail.com>
2025-07-08 03:10:25 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-07-07 19:10:24

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -10,17 +10,16 @@ var quotes = []string{
 	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 	"Life is what happens when you're busy making other plans. - John Lennon",
 	"The way to get started is to quit talking and begin doing. - Walt Disney",
-	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
 	"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
-	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-	"The only way to do great work is to love what you do. - Steve Jobs",
-	"If you can dream it, you can do it. - Walt Disney",
-	"Believe you can and you're halfway there. - Theodore Roosevelt",
-	"You miss 100% of the shots you don't take. - Wayne Gretzky",
+	"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. - Oprah Winfrey",
+	"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. - Helen Keller",
+	"Spread love everywhere you go. Let no one ever come to you without leaving happier. - Mother Teresa",
+	"When you reach the end of your rope, tie a knot in it and hang on. - Franklin D. Roosevelt",
+	"Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead",
+	"Don't judge each day by the harvest you reap but by the seeds that you plant. - Robert Louis Stevenson",
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	selectedQuote := quotes[rand.Intn(len(quotes))]
-	fmt.Println(selectedQuote)
+	fmt.Println(quotes[rand.Intn(len(quotes))])
 }
