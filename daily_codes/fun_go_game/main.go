430471940b3a4eb1e25a291d80f81f8c0b7247c3
wuweichi <wuduqi@gmail.com>
2025-07-08 02:10:22 +0800

    feat: 添加生成的代码 daily_codes/fun_go_game/main.go
    生成时间: 2025-07-07 18:10:21

--- daily_codes/fun_go_game/main.go
+++ daily_codes/fun_go_game/main.go
@@ -6,56 +6,34 @@ import (
 	"time"
 )
 
-type Player struct {
-	Name string
-	Health int
-	Attack int
-	Defense int
-}
-
-type Monster struct {
-	Name string
-	Health int
-	Attack int
-	Defense int
-}
-
-func (p *Player) AttackMonster(m *Monster) {
-	damage := p.Attack - m.Defense
-	if damage < 0 {
-		damage = 0
-	}
-	m.Health -= damage
-	fmt.Printf("%s attacks %s for %d damage!\n", p.Name, m.Name, damage)
-}
-
-func (m *Monster) AttackPlayer(p *Player) {
-	damage := m.Attack - p.Defense
-	if damage < 0 {
-		damage = 0
-	}
-	p.Health -= damage
-	fmt.Printf("%s attacks %s for %d damage!\n", m.Name, p.Name, damage)
-}
-
 func main() {
+	// Seed the random number generator
 	rand.Seed(time.Now().UnixNano())
-	player := Player{Name: "Hero", Health: 100, Attack: 20, Defense: 10}
-	monster := Monster{Name: "Goblin", Health: 50, Attack: 15, Defense: 5}
 
-	fmt.Println("A wild", monster.Name, "appears!")
-	for player.Health > 0 && monster.Health > 0 {
-		player.AttackMonster(&monster)
-		if monster.Health <= 0 {
-			fmt.Println(monster.Name, "has been defeated!")
+	// Generate a random number between 1 and 100
+	target := rand.Intn(100) + 1
+
+	fmt.Println("I've chosen a random number between 1 and 100. Can you guess it?")
+
+	var guess int
+	attempts := 0
+
+	for {
+		fmt.Print("Enter your guess: ")
+		_, err := fmt.Scanf("%d", &guess)
+		if err != nil {
+			fmt.Println("Please enter a valid number!")
+			continue
+		}
+		attempts++
+
+		if guess < target {
+			fmt.Println("Too low! Try again.")
+		} else if guess > target {
+			fmt.Println("Too high! Try again.")
+		} else {
+			fmt.Printf("Congratulations! You've guessed the number in %d attempts!\n", attempts)
 			break
 		}
-		monster.AttackPlayer(&player)
-		if player.Health <= 0 {
-			fmt.Println(player.Name, "has been defeated!")
-			break
-		}
-		fmt.Printf("%s: %d HP, %s: %d HP\n", player.Name, player.Health, monster.Name, monster.Health)
-		time.Sleep(1 * time.Second)
 	}
 }
