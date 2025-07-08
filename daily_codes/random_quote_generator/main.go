50289068ef0df2dd3003db407bc5e7ff3bf284f4
wuweichi <wuduqi@gmail.com>
2025-07-09 02:10:20 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-07-08 18:10:19

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -11,12 +11,12 @@ var quotes = []string{
 	"Life is what happens when you're busy making other plans. - John Lennon",
 	"The way to get started is to quit talking and begin doing. - Walt Disney",
 	"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
-	"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. - Oprah Winfrey",
+	"If you look at what you have in life, you'll always have more. - Oprah Winfrey",
 	"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. - Helen Keller",
-	"Spread love everywhere you go. Let no one ever come to you without leaving happier. - Mother Teresa",
-	"When you reach the end of your rope, tie a knot in it and hang on. - Franklin D. Roosevelt",
-	"Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead",
-	"Don't judge each day by the harvest you reap but by the seeds that you plant. - Robert Louis Stevenson",
+	"Whoever is happy will make others happy too. - Anne Frank",
+	"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
+	"You will face many defeats in life, but never let yourself be defeated. - Maya Angelou",
+	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
 }
 
 func main() {
