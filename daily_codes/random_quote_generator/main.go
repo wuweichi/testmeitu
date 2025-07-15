1cfffadba726f6c6643585d738951369dee1c01c
wuweichi <wuduqi@gmail.com>
2025-07-16 02:10:25 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-07-15 18:10:24

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -6,20 +6,20 @@ import (
 	"time"
 )
 
-var quotes = []string{
-	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-	"The secret of getting ahead is getting started. - Mark Twain",
-	"You miss 100% of the shots you don’t take. - Wayne Gretzky",
-	"Whether you think you can or you think you can’t, you’re right. - Henry Ford",
-	"The best time to plant a tree was 20 years ago. The second best time is now. - Chinese Proverb",
-	"It’s not whether you get knocked down, it’s whether you get up. - Vince Lombardi",
-	"If you’re going through hell, keep going. - Winston Churchill",
-	"People who are crazy enough to think they can change the world, are the ones who do. - Rob Siltanen",
-	"We may encounter many defeats but we must not be defeated. - Maya Angelou",
-}
-
 func main() {
+	quotes := []string{
+		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+		"Life is what happens when you're busy making other plans. - John Lennon",
+		"The way to get started is to quit talking and begin doing. - Walt Disney",
+		"Whoever is happy will make others happy too. - Anne Frank",
+		"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
+		"You only live once, but if you do it right, once is enough. - Mae West",
+		"Never let the fear of striking out keep you from playing the game. - Babe Ruth",
+		"Life is either a daring adventure or nothing at all. - Helen Keller",
+		"Many of life's failures are people who did not realize how close they were to success when they gave up. - Thomas A. Edison",
+		"You miss 100% of the shots you don't take. - Wayne Gretzky",
+	}
+
 	rand.Seed(time.Now().UnixNano())
 	randomIndex := rand.Intn(len(quotes))
 	fmt.Println(quotes[randomIndex])
