cf6198b063bcc1645d3a6b35dbb650555214013b
wuweichi <wuduqi@gmail.com>
2025-07-17 02:10:27 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-07-16 18:10:25

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -6,21 +6,21 @@ import (
 	"time"
 )
 
-func main() {
-	quotes := []string{
-		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-		"Life is what happens when you're busy making other plans. - John Lennon",
-		"The way to get started is to quit talking and begin doing. - Walt Disney",
-		"Whoever is happy will make others happy too. - Anne Frank",
-		"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-		"You only live once, but if you do it right, once is enough. - Mae West",
-		"Never let the fear of striking out keep you from playing the game. - Babe Ruth",
-		"Life is either a daring adventure or nothing at all. - Helen Keller",
-		"Many of life's failures are people who did not realize how close they were to success when they gave up. - Thomas A. Edison",
-		"You miss 100% of the shots you don't take. - Wayne Gretzky",
-	}
+var quotes = []string{
+	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+	"Life is what happens when you're busy making other plans. - John Lennon",
+	"The way to get started is to quit talking and begin doing. - Walt Disney",
+	"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
+	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
+	"The only way to do great work is to love what you do. - Steve Jobs",
+	"If you can dream it, you can do it. - Walt Disney",
+	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+	"You must be the change you wish to see in the world. - Mahatma Gandhi",
+	"Strive not to be a success, but rather to be of value. - Albert Einstein",
+}
 
+func main() {
 	rand.Seed(time.Now().UnixNano())
-	randomIndex := rand.Intn(len(quotes))
-	fmt.Println(quotes[randomIndex])
+	quote := quotes[rand.Intn(len(quotes))]
+	fmt.Println(quote)
 }
