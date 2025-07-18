0ecf4303342489d8145e495a6e4941c950c74893
wuweichi <wuduqi@gmail.com>
2025-07-19 02:10:30 +0800

    feat: 添加生成的代码 daily_codes/complex_clock/main.go
    生成时间: 2025-07-18 18:10:29

--- daily_codes/complex_clock/main.go
+++ daily_codes/complex_clock/main.go
@@ -3,52 +3,58 @@ package main
 import (
 	"fmt"
 	"time"
-	"github.com/inancgumus/screen"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+type Clock struct {
+	hours, minutes, seconds int
+}
+
+func (c *Clock) Update() {
+	now := time.Now()
+	c.hours = now.Hour()
+	c.minutes = now.Minute()
+	c.seconds = now.Second()
+}
+
+func (c *Clock) Display() {
+	fmt.Printf("\r%02d:%02d:%02d", c.hours, c.minutes, c.seconds)
+}
+
+func generateRandomColor() string {
+	colors := []string{
+		"\033[31m", // Red
+		"\033[32m", // Green
+		"\033[33m", // Yellow
+		"\033[34m", // Blue
+		"\033[35m", // Magenta
+		"\033[36m", // Cyan
+	}
+	return colors[rand.Intn(len(colors))]
+}
+
 func main() {
+	clock := Clock{}
+	rand.Seed(time.Now().UnixNano())
+
+	// Handle interrupt signal for graceful shutdown
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-interrupt
+		fmt.Println("\nExiting...")
+		os.Exit(0)
+	}()
+
 	for {
-		screen.Clear()
-		screen.MoveTopLeft()
-		now := time.Now()
-		hour, min, sec := now.Hour(), now.Minute(), now.Second()
-
-		clock := [...]placeholder{
-			digits[hour/10], digits[hour%10],
-			colon,
-			digits[min/10], digits[min%10],
-			colon,
-			digits[sec/10], digits[sec%10],
-		}
-
-		for line := range clock[0] {
-			for index, digit := range clock {
-				next := clock[index][line]
-				if digit == colon && sec%2 == 0 {
-					next = "   "
-				}
-				fmt.Print(next, "  ")
-			}
-			fmt.Println()
-		}
-		fmt.Println()
-		time.Sleep(time.Second)
+		clock.Update()
+		color := generateRandomColor()
+		fmt.Print(color)
+		clock.Display()
+		time.Sleep(1 * time.Second)
 	}
 }
-
-var digits = [...]placeholder{
-	{" _ ", "| |", "|_|"},
-	{"   ", "  |", "  |"},
-	{" _ ", " _|", "|_ "},
-	{" _ ", " _|", " _|"},
-	{"   ", "|_|", "  |"},
-	{" _ ", "|_ ", " _|"},
-	{" _ ", "|_ ", "|_|"},
-	{" _ ", "  |", "  |"},
-	{" _ ", "|_|", "|_|"},
-	{" _ ", "|_|", " _|"},
-}
-
-var colon = placeholder{"   ", " . ", " . "}
-
-type placeholder [3]string
