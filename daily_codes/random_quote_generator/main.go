98f1a0a601723a289ca18bc8fb0c6264decfa982
wuweichi <wuduqi@gmail.com>
2025-07-20 02:10:19 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-07-19 18:10:18

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -10,13 +10,8 @@ var quotes = []string{
 	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 	"Life is what happens when you're busy making other plans. - John Lennon",
 	"The way to get started is to quit talking and begin doing. - Walt Disney",
-	"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
+	"Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
 	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-	"You miss 100% of the shots you don’t take. - Wayne Gretzky",
-	"The only way to do great work is to love what you do. - Steve Jobs",
-	"If you can dream it, you can do it. - Walt Disney",
-	"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
 }
 
 func main() {
