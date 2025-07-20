0a789b77f6d65b94d59427138d5bf1a4cac69364
wuweichi <wuduqi@gmail.com>
2025-07-21 02:10:24 +0800

    feat: 添加生成的代码 daily_codes/fun_with_go/main.go
    生成时间: 2025-07-20 18:10:23

--- daily_codes/fun_with_go/main.go
+++ daily_codes/fun_with_go/main.go
@@ -7,27 +7,32 @@ import (
 )
 
 func main() {
+	// Seed the random number generator
 	rand.Seed(time.Now().UnixNano())
-	secretNumber := rand.Intn(100) + 1
-	fmt.Println("I've chosen a random number between 1 and 100. Can you guess it?")
+
+	// Generate a random number between 0 and 100
+	randomNumber := rand.Intn(101)
+
+	fmt.Printf("I'm thinking of a number between 0 and 100. Can you guess it?\n")
 
 	var guess int
 	attempts := 0
+
 	for {
-		fmt.Print("Your guess: ")
-		_, err := fmt.Scan(&guess)
+		fmt.Print("Enter your guess: ")
+		_, err := fmt.Scanf("%d", &guess)
 		if err != nil {
 			fmt.Println("Please enter a valid number!")
 			continue
 		}
 		attempts++
 
-		if guess < secretNumber {
+		if guess < randomNumber {
 			fmt.Println("Too low! Try again.")
-		} else if guess > secretNumber {
+		} else if guess > randomNumber {
 			fmt.Println("Too high! Try again.")
 		} else {
-			fmt.Printf("Congratulations! You've guessed the number in %d attempts!\n", attempts)
+			fmt.Printf("Congratulations! You guessed the number in %d attempts!\n", attempts)
 			break
 		}
 	}
