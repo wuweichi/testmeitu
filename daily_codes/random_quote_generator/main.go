242f1d7b336a17c9c3f88ef7c8a502ed657e8d46
wuweichi <wuduqi@gmail.com>
2025-07-26 02:10:25 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-07-25 18:10:24

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -6,21 +6,20 @@ import (
 	"time"
 )
 
-func main() {
-	quotes := []string{
-		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-		"Life is what happens when you're busy making other plans. - John Lennon",
-		"The way to get started is to quit talking and begin doing. - Walt Disney",
-		"Your time is limited, don't waste it living someone else's life. - Steve Jobs",
-		"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-		"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-		"You must be the change you wish to see in the world. - Mahatma Gandhi",
-		"Whatever you are, be a good one. - Abraham Lincoln",
-		"The best way to predict the future is to invent it. - Alan Kay",
-		"Strive not to be a success, but rather to be of value. - Albert Einstein",
-	}
+var quotes = []string{
+	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+	"Life is what happens when you're busy making other plans. - John Lennon",
+	"It is during our darkest moments that we must focus to see the light. - Aristotle",
+	"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
+	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+	"You must be the change you wish to see in the world. - Mahatma Gandhi",
+	"In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
+	"You only live once, but if you do it right, once is enough. - Mae West",
+	"The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+	"Life is either a daring adventure or nothing at all. - Helen Keller",
+}
 
+func main() {
 	rand.Seed(time.Now().UnixNano())
-	randomQuote := quotes[rand.Intn(len(quotes))]
-	fmt.Println(randomQuote)
+	fmt.Println(quotes[rand.Intn(len(quotes))])
 }
