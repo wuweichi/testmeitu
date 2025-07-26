74c840cbee7515e563a3200f52ffa4834793e921
wuweichi <wuduqi@gmail.com>
2025-07-27 02:10:24 +0800

    feat: 添加生成的代码 daily_codes/simple_http_server/main.go
    生成时间: 2025-07-26 18:10:23

--- daily_codes/simple_http_server/main.go
+++ daily_codes/simple_http_server/main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now().Format(time.RFC1123)
+		fmt.Fprintf(w, "Current time is: %s\n", currentTime)
+	})
 
+	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			name = "Guest"
+		}
+		fmt.Fprintf(w, "Hello, %s!\n", name)
+	})
+
+	fmt.Println("Starting server on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
