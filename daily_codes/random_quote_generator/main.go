99c029074b33f5f776d9dbe93107b43572d321e6
wuweichi <wuduqi@gmail.com>
2025-08-01 03:10:26 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-07-31 19:10:25

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -6,21 +6,21 @@ import (
 	"time"
 )
 
-func main() {
-	quotes := []string{
-		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-		"Life is what happens when you're busy making other plans. - John Lennon",
-		"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-		"It is during our darkest moments that we must focus to see the light. - Aristotle",
-		"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
-		"You will face many defeats in life, but never let yourself be defeated. - Maya Angelou",
-		"The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
-		"In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
-		"Life is either a daring adventure or nothing at all. - Helen Keller",
-		"Many of life's failures are people who did not realize how close they were to success when they gave up. - Thomas A. Edison",
-	}
+var quotes = []string{
+	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+	"Life is what happens when you're busy making other plans. - John Lennon",
+	"In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
+	"You must be the change you wish to see in the world. - Mahatma Gandhi",
+	"Spread love everywhere you go. Let no one ever come to you without leaving happier. - Mother Teresa",
+	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+	"Tell me and I forget. Teach me and I remember. Involve me and I learn. - Benjamin Franklin",
+	"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
+	"It is during our darkest moments that we must focus to see the light. - Aristotle",
+	"Whoever is happy will make others happy too. - Anne Frank",
+	"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment. - Buddha",
+}
 
+func main() {
 	rand.Seed(time.Now().UnixNano())
-	randomIndex := rand.Intn(len(quotes))
-	fmt.Println(quotes[randomIndex])
+	fmt.Println(quotes[rand.Intn(len(quotes))])
 }
