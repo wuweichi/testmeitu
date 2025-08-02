358f20e6e6d4225c93f8c89f238c2d34bc324673
wuweichi <wuduqi@gmail.com>
2025-08-03 02:10:25 +0800

    feat: 添加生成的代码 daily_codes/fun_with_go/main.go
    生成时间: 2025-08-02 18:10:24

--- daily_codes/fun_with_go/main.go
+++ daily_codes/fun_with_go/main.go
@@ -13,11 +13,14 @@ func main() {
 	// Generate a random number between 0 and 100
 	randomNumber := rand.Intn(101)
 
-	fmt.Printf("I'm thinking of a number between 0 and 100. Can you guess it?\n")
+	// Print a welcome message
+	fmt.Println("Welcome to the Fun with Go program!")
+	fmt.Println("I'm thinking of a number between 0 and 100. Can you guess it?")
 
 	var guess int
 	attempts := 0
 
+	// Loop until the user guesses the number
 	for {
 		fmt.Print("Enter your guess: ")
 		_, err := fmt.Scanf("%d", &guess)
@@ -32,7 +35,7 @@ func main() {
 		} else if guess > randomNumber {
 			fmt.Println("Too high! Try again.")
 		} else {
-			fmt.Printf("Congratulations! You guessed the number in %d attempts!\n", attempts)
+			fmt.Printf("Congratulations! You've guessed the number in %d attempts!\n", attempts)
 			break
 		}
 	}
