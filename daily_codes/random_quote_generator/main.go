a0140f820bedc008532d57f59660eb2c42c235b6
wuweichi <wuduqi@gmail.com>
2025-08-05 02:10:24 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-08-04 18:10:23

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -11,13 +11,13 @@ func main() {
 		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 		"Life is what happens when you're busy making other plans. - John Lennon",
 		"The way to get started is to quit talking and begin doing. - Walt Disney",
+		"Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+		"Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+		"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
 		"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
-		"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. - Oprah Winfrey",
-		"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. - Helen Keller",
-		"Spread love everywhere you go. Let no one ever come to you without leaving happier. - Mother Teresa",
-		"When you reach the end of your rope, tie a knot in it and hang on. - Franklin D. Roosevelt",
-		"Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead",
-		"Don't judge each day by the harvest you reap but by the seeds that you plant. - Robert Louis Stevenson",
+		"If you can dream it, you can do it. - Walt Disney",
+		"The best way to predict the future is to invent it. - Alan Kay",
+		"Strive not to be a success, but rather to be of value. - Albert Einstein",
 	}
 
 	rand.Seed(time.Now().UnixNano())
