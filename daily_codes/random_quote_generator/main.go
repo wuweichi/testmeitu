c8e5464d3a859f9bc6dd00a048d9d21890868f75
wuweichi <wuduqi@gmail.com>
2025-08-07 03:10:20 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-08-06 19:10:19

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -12,15 +12,11 @@ var quotes = []string{
 	"The way to get started is to quit talking and begin doing. - Walt Disney",
 	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
 	"It is during our darkest moments that we must focus to see the light. - Aristotle",
+	"Whoever is happy will make others happy too. - Anne Frank",
 	"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
-	"You will face many defeats in life, but never let yourself be defeated. - Maya Angelou",
-	"The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
-	"In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
-	"Never let the fear of striking out keep you from playing the game. - Babe Ruth",
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	quote := quotes[rand.Intn(len(quotes))]
-	fmt.Println(quote)
+	fmt.Println(quotes[rand.Intn(len(quotes))])
 }
