5668279e57657701eefdc982eb5b4bd11d2d3110
wuweichi <wuduqi@gmail.com>
2025-08-10 03:10:27 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-08-09 19:10:26

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -6,17 +6,18 @@ import (
 	"time"
 )
 
-var quotes = []string{
-	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-	"Life is what happens when you're busy making other plans. - John Lennon",
-	"The way to get started is to quit talking and begin doing. - Walt Disney",
-	"Your time is limited, don't waste it living someone else's life. - Steve Jobs",
-	"It is during our darkest moments that we must focus to see the light. - Aristotle",
-	"Whoever is happy will make others happy too. - Anne Frank",
-	"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
-}
-
 func main() {
+	quotes := []string{
+		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+		"Life is what happens when you're busy making other plans. - John Lennon",
+		"The way to get started is to quit talking and begin doing. - Walt Disney",
+		"Whoever is happy will make others happy too. - Anne Frank",
+		"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
+		"Life is either a daring adventure or nothing at all. - Helen Keller",
+		"You will face many defeats in life, but never let yourself be defeated. - Maya Angelou",
+	}
+
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println(quotes[rand.Intn(len(quotes))])
+	randomIndex := rand.Intn(len(quotes))
+	fmt.Println(quotes[randomIndex])
 }
