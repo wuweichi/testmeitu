ce4e5959252339004a24dc832750d936b54ed332
wuweichi <wuduqi@gmail.com>
2025-08-10 02:10:21 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-08-09 18:10:20

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -10,7 +10,7 @@ var quotes = []string{
 	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 	"Life is what happens when you're busy making other plans. - John Lennon",
 	"The way to get started is to quit talking and begin doing. - Walt Disney",
-	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+	"Your time is limited, don't waste it living someone else's life. - Steve Jobs",
 	"It is during our darkest moments that we must focus to see the light. - Aristotle",
 	"Whoever is happy will make others happy too. - Anne Frank",
 	"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
