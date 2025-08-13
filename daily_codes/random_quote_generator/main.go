3c8ebee4bf119d17b170822e88a709e3ae986da3
wuweichi <wuduqi@gmail.com>
2025-08-14 02:10:26 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-08-13 18:10:25

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -11,10 +11,13 @@ func main() {
 		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 		"Life is what happens when you're busy making other plans. - John Lennon",
 		"The way to get started is to quit talking and begin doing. - Walt Disney",
-		"Whoever is happy will make others happy too. - Anne Frank",
+		"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
 		"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-		"Life is either a daring adventure or nothing at all. - Helen Keller",
-		"You will face many defeats in life, but never let yourself be defeated. - Maya Angelou",
+		"The only way to do great work is to love what you do. - Steve Jobs",
+		"If you can dream it, you can do it. - Walt Disney",
+		"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+		"Believe you can and you're halfway there. - Theodore Roosevelt",
+		"You miss 100% of the shots you don't take. - Wayne Gretzky",
 	}
 
 	rand.Seed(time.Now().UnixNano())
