b48b8b1181328d81c3225ddc5150d89e2a7b32d6
wuweichi <wuduqi@gmail.com>
2025-08-16 02:10:22 +0800

    feat: 添加生成的代码 daily_codes/fun_go_program/main.go
    生成时间: 2025-08-15 18:10:20

--- daily_codes/fun_go_program/main.go
+++ daily_codes/fun_go_program/main.go
@@ -8,9 +8,27 @@ import (
 
 func main() {
 	fmt.Println("Welcome to the Fun Go Program!")
-	fmt.Println("Generating a random number between 1 and 100...")
+	fmt.Println("This program generates random numbers and checks if they're prime.")
+
 	rand.Seed(time.Now().UnixNano())
-	randomNumber := rand.Intn(100) + 1
-	fmt.Printf("The random number is: %d\n", randomNumber)
-	fmt.Println("Thanks for using the Fun Go Program!")
+	for i := 0; i < 10; i++ {
+		n := rand.Intn(100) + 1
+		if isPrime(n) {
+			fmt.Printf("%d is a prime number.\n", n)
+		} else {
+			fmt.Printf("%d is not a prime number.\n", n)
+		}
+	}
+}
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
