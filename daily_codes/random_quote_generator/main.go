25eae368e376cf94f1c5b5ef06dab1f3038d6ce8
wuweichi <wuduqi@gmail.com>
2025-08-18 03:10:25 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-08-17 19:10:24

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -10,13 +10,13 @@ var quotes = []string{
 	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 	"Life is what happens when you're busy making other plans. - John Lennon",
 	"The way to get started is to quit talking and begin doing. - Walt Disney",
-	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
 	"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
-	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
-	"The only way to do great work is to love what you do. - Steve Jobs",
-	"If you can dream it, you can do it. - Walt Disney",
-	"Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer",
-	"Believe you can and you're halfway there. - Theodore Roosevelt",
+	"If you look at what you have in life, you'll always have more. - Oprah Winfrey",
+	"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. - Helen Keller",
+	"It is during our darkest moments that we must focus to see the light. - Aristotle",
+	"Whoever is happy will make others happy too. - Anne Frank",
+	"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
+	"You will face many defeats in life, but never let yourself be defeated. - Maya Angelou",
 }
 
 func main() {
