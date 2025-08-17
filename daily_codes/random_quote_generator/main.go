7fa82697d7d6c341baced9a9d34909a874b3f80d
wuweichi <wuduqi@gmail.com>
2025-08-18 02:10:25 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-08-17 18:10:24

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -9,14 +9,14 @@ import (
 var quotes = []string{
 	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 	"Life is what happens when you're busy making other plans. - John Lennon",
+	"The way to get started is to quit talking and begin doing. - Walt Disney",
 	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
 	"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
 	"Do not watch the clock. Do what it does. Keep going. - Sam Levenson",
 	"The only way to do great work is to love what you do. - Steve Jobs",
 	"If you can dream it, you can do it. - Walt Disney",
-	"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+	"Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer",
 	"Believe you can and you're halfway there. - Theodore Roosevelt",
-	"You miss 100% of the shots you don't take. - Wayne Gretzky",
 }
 
 func main() {
