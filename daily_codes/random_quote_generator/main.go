e6501e28b612294a752aeb278c1865e8e869aec7
wuweichi <wuduqi@gmail.com>
2025-08-19 02:10:20 +0800

    feat: 添加生成的代码 daily_codes/random_quote_generator/main.go
    生成时间: 2025-08-18 18:10:19

--- daily_codes/random_quote_generator/main.go
+++ daily_codes/random_quote_generator/main.go
@@ -6,20 +6,21 @@ import (
 	"time"
 )
 
-var quotes = []string{
-	"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-	"Life is what happens when you're busy making other plans. - John Lennon",
-	"The way to get started is to quit talking and begin doing. - Walt Disney",
-	"It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
-	"If you look at what you have in life, you'll always have more. - Oprah Winfrey",
-	"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. - Helen Keller",
-	"It is during our darkest moments that we must focus to see the light. - Aristotle",
-	"Whoever is happy will make others happy too. - Anne Frank",
-	"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
-	"You will face many defeats in life, but never let yourself be defeated. - Maya Angelou",
-}
-
 func main() {
+	quotes := []string{
+		"The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+		"Life is what happens when you're busy making other plans. - John Lennon",
+		"The way to get started is to quit talking and begin doing. - Walt Disney",
+		"Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+		"It is during our darkest moments that we must focus to see the light. - Aristotle",
+		"Whoever is happy will make others happy too. - Anne Frank",
+		"Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
+		"You will face many defeats in life, but never let yourself be defeated. - Maya Angelou",
+		"The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+		"In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
+	}
+
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println(quotes[rand.Intn(len(quotes))])
+	randomQuote := quotes[rand.Intn(len(quotes))]
+	fmt.Println(randomQuote)
 }
