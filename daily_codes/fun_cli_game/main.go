867f47ca36e259b6db2d055d55db7273ae073674
wuweichi <wuduqi@gmail.com>
2025-09-03 02:12:10 +0800

    feat: 添加生成的代码 daily_codes/fun_cli_game/main.go
    生成时间: 2025-09-02 18:12:09

--- daily_codes/fun_cli_game/main.go
+++ daily_codes/fun_cli_game/main.go
@@ -10,589 +10,258 @@ import (
 	"time"
 )
 
-// Player represents a player in the game
+// Player struct to hold player information
 type Player struct {
-	Name  string
-	Score int
+	Name     string
+	Health   int
+	Strength int
+	Level    int
+	Exp      int
 }
 
-// GameState holds the current state of the game
+// Enemy struct to hold enemy information
+type Enemy struct {
+	Name     string
+	Health   int
+	Strength int
+	Level    int
+}
+
+// Item struct to hold item information
+type Item struct {
+	Name        string
+	Description string
+	Effect      func(*Player)
+}
+
+// GameState struct to manage game state
 type GameState struct {
-	Players      []Player
-	CurrentRound int
-	MaxRounds    int
+	Player      *Player
+	Enemies     []*Enemy
+	Inventory   []*Item
+	GameRunning bool
 }
 
-// initGame initializes the game state
-func initGame() GameState {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter number of players: ")
-	numPlayersStr, _ := reader.ReadString('\n')
-	numPlayersStr = strings.TrimSpace(numPlayersStr)
-	numPlayers, _ := strconv.Atoi(numPlayersStr)
-	if numPlayers < 1 {
-		numPlayers = 1
+// InitializePlayer creates a new player with default values
+func InitializePlayer(name string) *Player {
+	return &Player{
+		Name:     name,
+		Health:   100,
+		Strength: 10,
+		Level:    1,
+		Exp:      0,
 	}
+}
 
-	players := make([]Player, numPlayers)
-	for i := 0; i < numPlayers; i++ {
-		fmt.Printf("Enter name for player %d: ", i+1)
-		name, _ := reader.ReadString('\n')
-		name = strings.TrimSpace(name)
-		if name == "" {
-			name = fmt.Sprintf("Player%d", i+1)
+// InitializeEnemies creates a list of enemies
+func InitializeEnemies() []*Enemy {
+	return []*Enemy{
+		{Name: "Goblin", Health: 30, Strength: 5, Level: 1},
+		{Name: "Orc", Health: 50, Strength: 8, Level: 2},
+		{Name: "Dragon", Health: 100, Strength: 15, Level: 3},
+	}
+}
+
+// InitializeItems creates a list of items
+func InitializeItems() []*Item {
+	return []*Item{
+		{
+			Name:        "Health Potion",
+			Description: "Restores 20 health points.",
+			Effect: func(p *Player) {
+				p.Health += 20
+				fmt.Println("You used a Health Potion and restored 20 health!")
+			},
+		},
+		{
+			Name:        "Strength Elixir",
+			Description: "Increases strength by 5 points.",
+			Effect: func(p *Player) {
+				p.Strength += 5
+				fmt.Println("You used a Strength Elixir and gained 5 strength!")
+			},
+		},
+	}
+}
+
+// Attack simulates an attack between two entities
+func Attack(attacker, defender interface{}) {
+	switch a := attacker.(type) {
+	case *Player:
+		switch d := defender.(type) {
+		case *Enemy:
+			damage := a.Strength + rand.Intn(5) // Random damage variation
+			d.Health -= damage
+			fmt.Printf("%s attacks %s for %d damage!\n", a.Name, d.Name, damage)
 		}
-		players[i] = Player{Name: name, Score: 0}
-	}
-
-	fmt.Print("Enter number of rounds: ")
-	roundsStr, _ := reader.ReadString('\n')
-	roundsStr = strings.TrimSpace(roundsStr)
-	maxRounds, _ := strconv.Atoi(roundsStr)
-	if maxRounds < 1 {
-		maxRounds = 5
-	}
-
-	return GameState{
-		Players:      players,
-		CurrentRound: 1,
-		MaxRounds:    maxRounds,
-	}
-}
-
-// playRound simulates a round of the game
-func playRound(gs *GameState) {
-	fmt.Printf("\n--- Round %d ---\n", gs.CurrentRound)
-	reader := bufio.NewReader(os.Stdin)
-	for i := range gs.Players {
-		fmt.Printf("%s, press Enter to roll the dice...", gs.Players[i].Name)
-		reader.ReadString('\n')
-		roll := rand.Intn(6) + 1
-		fmt.Printf("You rolled a %d!\n", roll)
-		gs.Players[i].Score += roll
-	}
-	gs.CurrentRound++
-}
-
-// displayScores shows the current scores
-func displayScores(gs GameState) {
-	fmt.Println("\nCurrent Scores:")
-	for _, player := range gs.Players {
-		fmt.Printf("%s: %d\n", player.Name, player.Score)
-	}
-}
-
-// determineWinner finds and announces the winner
-func determineWinner(gs GameState) {
-	maxScore := -1
-	winners := []string{}
-	for _, player := range gs.Players {
-		if player.Score > maxScore {
-			maxScore = player.Score
-			winners = []string{player.Name}
-		} else if player.Score == maxScore {
-			winners = append(winners, player.Name)
-		}
-	}
-	if len(winners) == 1 {
-		fmt.Printf("\nWinner: %s with %d points!\n", winners[0], maxScore)
-	} else {
-		fmt.Printf("\nIt's a tie between %s with %d points!\n", strings.Join(winners, ", "), maxScore)
-	}
-}
-
-// helperFunction1 is a placeholder for additional functionality
-func helperFunction1() {
-	// This function does nothing, just to add lines
-	for i := 0; i < 10; i++ {
-		_ = i * 2
-	}
-}
-
-// helperFunction2 is another placeholder
-func helperFunction2() {
-	// More placeholder code
-	arr := []int{1, 2, 3, 4, 5}
-	for _, val := range arr {
-		_ = val + 1
-	}
-}
-
-// helperFunction3 placeholder
-func helperFunction3() {
-	str := "example"
-	for j := 0; j < len(str); j++ {
-		_ = string(str[j])
-	}
-}
-
-// helperFunction4 placeholder
-func helperFunction4() {
-	// Dummy calculations
-	x := 10
-	y := 20
-	_ = x + y
-	_ = x * y
-}
-
-// helperFunction5 placeholder
-func helperFunction5() {
-	// Loop to add lines
-	for k := 0; k < 5; k++ {
-		_ = k
-	}
-}
-
-// helperFunction6 placeholder
-func helperFunction6() {
-	// Simple if-else
-	a := true
-	if a {
-		_ = "true"
-	} else {
-		_ = "false"
-	}
-}
-
-// helperFunction7 placeholder
-func helperFunction7() {
-	// Switch statement
-	num := 3
-	switch num {
-	case 1:
-		_ = "one"
-	case 2:
-		_ = "two"
-	default:
-		_ = "other"
-	}
-}
-
-// helperFunction8 placeholder
-func helperFunction8() {
-	// Array manipulation
-	arr := [3]int{1, 2, 3}
-	for idx := range arr {
-		arr[idx] += 1
-	}
-}
-
-// helperFunction9 placeholder
-func helperFunction9() {
-	// Slice operations
-	slice := make([]string, 2)
-	slice[0] = "hello"
-	slice[1] = "world"
-	_ = slice
-}
-
-// helperFunction10 placeholder
-func helperFunction10() {
-	// Map usage
-	m := map[string]int{"a": 1, "b": 2}
-	for key, value := range m {
-		_ = key
-		_ = value
-	}
-}
-
-// helperFunction11 placeholder
-func helperFunction11() {
-	// Function calls within
-	helperFunction1()
-	helperFunction2()
-}
-
-// helperFunction12 placeholder
-func helperFunction12() {
-	// More loops
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			_ = i + j
+	case *Enemy:
+		switch d := defender.(type) {
+		case *Player:
+			damage := a.Strength + rand.Intn(5)
+			d.Health -= damage
+			fmt.Printf("%s attacks %s for %d damage!\n", a.Name, d.Name, damage)
 		}
 	}
 }
 
-// helperFunction13 placeholder
-func helperFunction13() {
-	// Defer example
-	defer func() {
-		_ = "deferred"
-	}()
-}
-
-// helperFunction14 placeholder
-func helperFunction14() {
-	// Channel simple
-	ch := make(chan int, 1)
-	ch <- 1
-	<-ch
-}
-
-// helperFunction15 placeholder
-func helperFunction15() {
-	// Goroutine simple
-	go func() {
-		_ = "goroutine"
-	}()
-}
-
-// helperFunction16 placeholder
-func helperFunction16() {
-	// Error handling dummy
-	_, err := strconv.Atoi("123")
-	if err != nil {
-		_ = err.Error()
+// CheckLevelUp checks if the player should level up
+func CheckLevelUp(p *Player) {
+	if p.Exp >= p.Level*100 {
+		p.Level++
+		p.Health += 20
+		p.Strength += 5
+		fmt.Printf("Level up! You are now level %d. Health and strength increased.\n", p.Level)
+		p.Exp = 0
 	}
 }
 
-// helperFunction17 placeholder
-func helperFunction17() {
-	// Time usage
-	_ = time.Now().Unix()
+// DisplayStatus shows the player's current status
+func DisplayStatus(p *Player) {
+	fmt.Printf("Name: %s, Health: %d, Strength: %d, Level: %d, Exp: %d\n", p.Name, p.Health, p.Strength, p.Level, p.Exp)
 }
 
-// helperFunction18 placeholder
-func helperFunction18() {
-	// String functions
-	s := "test"
-	_ = strings.ToUpper(s)
-}
-
-// helperFunction19 placeholder
-func helperFunction19() {
-	// Math operations
-	_ = rand.Float64()
-}
-
-// helperFunction20 placeholder
-func helperFunction20() {
-	// Nested function
-	func() {
-		_ = "nested"
-	}()
-}
-
-// helperFunction21 placeholder
-func helperFunction21() {
-	// Pointer example
-	var x int = 5
-	ptr := &x
-	_ = *ptr
-}
-
-// helperFunction22 placeholder
-func helperFunction22() {
-	// Struct dummy
-	type Temp struct {
-		A int
-		B string
+// DisplayInventory shows the player's inventory
+func DisplayInventory(inventory []*Item) {
+	if len(inventory) == 0 {
+		fmt.Println("Your inventory is empty.")
+		return
 	}
-	t := Temp{A: 1, B: "b"}
-	_ = t
-}
-
-// helperFunction23 placeholder
-func helperFunction23() {
-	// Interface dummy
-	var i interface{} = "hello"
-	_ = i
-}
-
-// helperFunction24 placeholder
-func helperFunction24() {
-	// Type assertion
-	var i interface{} = 42
-	if val, ok := i.(int); ok {
-		_ = val
+	fmt.Println("Inventory:")
+	for i, item := range inventory {
+		fmt.Printf("%d. %s - %s\n", i+1, item.Name, item.Description)
 	}
 }
 
-// helperFunction25 placeholder
-func helperFunction25() {
-	// Select with default
-	select {
-	default:
-		_ = "default"
+// UseItem allows the player to use an item from inventory
+func UseItem(inventory []*Item, p *Player, index int) []*Item {
+	if index < 0 || index >= len(inventory) {
+		fmt.Println("Invalid item selection.")
+		return inventory
 	}
+	item := inventory[index]
+	item.Effect(p)
+	// Remove the used item from inventory
+	return append(inventory[:index], inventory[index+1:]...)
 }
 
-// helperFunction26 placeholder
-func helperFunction26() {
-	// Buffered channel
-	ch := make(chan int, 2)
-	ch <- 1
-	ch <- 2
-	<-ch
-	<-ch
-}
-
-// helperFunction27 placeholder
-func helperFunction27() {
-	// WaitGroup dummy
-	// Note: In real code, sync.WaitGroup would be used, but here for lines
-	var wg struct{}
-	_ = wg
-}
-
-// helperFunction28 placeholder
-func helperFunction28() {
-	// JSON marshaling dummy
-	// Using a simple struct
-	type Data struct {
-		Name string
-		Age  int
-	}
-	d := Data{Name: "John", Age: 30}
-	_ = d
-}
-
-// helperFunction29 placeholder
-func helperFunction29() {
-	// Recursive function call
-	func rec(n int) int {
-		if n <= 1 {
-			return 1
-		}
-		return n * rec(n-1)
-	}
-	_ = rec(5)
-}
-
-// helperFunction30 placeholder
-func helperFunction30() {
-	// Panic and recover dummy
-	defer func() {
-		if r := recover(); r != nil {
-			_ = r
-		}
-	}()
-	panic("test panic")
-}
-
-// helperFunction31 placeholder
-func helperFunction31() {
-	// Custom error type
-	type MyError struct {
-		Msg string
-	}
-	func (e *MyError) Error() string {
-		return e.Msg
-	}
-	err := &MyError{Msg: "error"}
-	_ = err
-}
-
-// helperFunction32 placeholder
-func helperFunction32() {
-	// Reflection dummy
-	// Using reflect lightly
-	var x int = 10
-	_ = x
-	// reflect.TypeOf(x) // Not used to avoid import, just comment
-}
-
-// helperFunction33 placeholder
-func helperFunction33() {
-	// File I/O dummy
-	// os.Create("temp.txt") // Not actually creating file
-	_ = "file"
-}
-
-// helperFunction34 placeholder
-func helperFunction34() {
-	// Network dummy
-	// net.Dial("tcp", "example.com:80") // Not actually dialing
-	_ = "network"
-}
-
-// helperFunction35 placeholder
-func helperFunction35() {
-	// Database dummy
-	// sql.Open("driver", "dsn") // Not actually opening
-	_ = "database"
-}
-
-// helperFunction36 placeholder
-func helperFunction36() {
-	// Template dummy
-	// template.New("name") // Not actually using
-	_ = "template"
-}
-
-// helperFunction37 placeholder
-func helperFunction37() {
-	// Cryptographic dummy
-	// crypto/rand.Reader // Not actually using
-	_ = "crypto"
-}
-
-// helperFunction38 placeholder
-func helperFunction38() {
-	// Compression dummy
-	// compress/gzip.NewWriter // Not actually using
-	_ = "compression"
-}
-
-// helperFunction39 placeholder
-func helperFunction39() {
-	// Concurrency patterns dummy
-	// Using a mutex-like struct
-	var mu struct{}
-	_ = mu
-}
-
-// helperFunction40 placeholder
-func helperFunction40() {
-	// Algorithm dummy
-	// Sort a slice
-	arr := []int{3, 1, 4, 1, 5}
-	for i := 0; i < len(arr)-1; i++ {
-		for j := i + 1; j < len(arr); j++ {
-			if arr[i] > arr[j] {
-				arr[i], arr[j] = arr[j], arr[i]
+// Battle simulates a battle between player and an enemy
+func Battle(p *Player, e *Enemy) {
+	fmt.Printf("A wild %s appears!\n", e.Name)
+	for p.Health > 0 && e.Health > 0 {
+		fmt.Println("\nChoose an action: 1. Attack, 2. Use Item, 3. Run")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		switch input {
+		case "1":
+			Attack(p, e)
+			if e.Health <= 0 {
+				fmt.Printf("You defeated the %s!\n", e.Name)
+				p.Exp += e.Level * 20
+				CheckLevelUp(p)
+				return
 			}
+			Attack(e, p)
+			if p.Health <= 0 {
+				fmt.Println("You have been defeated! Game over.")
+				return
+			}
+		case "2":
+			// For simplicity, assume no inventory in battle; can extend
+			fmt.Println("No items available in battle in this version.")
+		case "3":
+			fmt.Println("You ran away safely.")
+			return
+		default:
+			fmt.Println("Invalid choice. Please choose 1, 2, or 3.")
 		}
 	}
-	_ = arr
 }
 
-// helperFunction41 placeholder
-func helperFunction41() {
-	// More placeholders to reach line count
-	for i := 0; i < 50; i++ {
-		_ = i
+// Explore allows the player to explore the game world
+func Explore(gs *GameState) {
+	fmt.Println("You are exploring...")
+	// Random event: encounter enemy or find item
+	event := rand.Intn(3)
+	switch event {
+	case 0:
+		fmt.Println("You found nothing.")
+	case 1:
+		enemyIndex := rand.Intn(len(gs.Enemies))
+		Battle(gs.Player, gs.Enemies[enemyIndex])
+	case 2:
+		if len(gs.Inventory) < 5 { // Limit inventory size
+			itemIndex := rand.Intn(len(InitializeItems()))
+			newItem := InitializeItems()[itemIndex]
+			gs.Inventory = append(gs.Inventory, newItem)
+			fmt.Printf("You found a %s!\n", newItem.Name)
+		} else {
+			fmt.Println("Your inventory is full. Cannot pick up more items.")
+		}
 	}
 }
 
-// helperFunction42 placeholder
-func helperFunction42() {
-	// Even more lines
-	str := ""
-	for i := 0; i < 10; i++ {
-		str += "a"
-	}
-	_ = str
-}
-
-// helperFunction43 placeholder
-func helperFunction43() {
-	// Dummy data processing
-	data := []float64{1.1, 2.2, 3.3}
-	sum := 0.0
-	for _, d := range data {
-		sum += d
-	}
-	_ = sum
-}
-
-// helperFunction44 placeholder
-func helperFunction44() {
-	// Mock logging
-	fmt.Print("")
-}
-
-// helperFunction45 placeholder
-func helperFunction45() {
-	// Conditional compilation dummy
-	// #ifdef etc., but in Go, use build tags or comments
-	_ = "build"
-}
-
-// helperFunction46 placeholder
-func helperFunction46() {
-	// Internationalization dummy
-	_ = "i18n"
-}
-
-// helperFunction47 placeholder
-func helperFunction47() {
-	// Testing dummy
-	// Would have test functions, but here for lines
-	_ = "test"
-}
-
-// helperFunction48 placeholder
-func helperFunction48() {
-	// Benchmark dummy
-	_ = "benchmark"
-}
-
-// helperFunction49 placeholder
-func helperFunction49() {
-	// Profiling dummy
-	_ = "profile"
-}
-
-// helperFunction50 placeholder
-func helperFunction50() {
-	// Final placeholder
-	_ = "end"
-}
-
-// main function is the entry point of the program
+// Main game loop
 func main() {
-	rand.Seed(time.Now().UnixNano()) // Seed the random number generator
-	gameState := initGame()
-	
-	for gameState.CurrentRound <= gameState.MaxRounds {
-		playRound(&gameState)
-		displayScores(gameState)
+	rand.Seed(time.Now().UnixNano())
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Welcome to the Fun CLI Game!")
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	player := InitializePlayer(name)
+	enemies := InitializeEnemies()
+	inventory := []*Item{}
+	gameState := &GameState{
+		Player:      player,
+		Enemies:     enemies,
+		Inventory:   inventory,
+		GameRunning: true,
 	}
-	
-	determineWinner(gameState)
-	
-	// Call placeholder functions to increase line count
-	helperFunction1()
-	helperFunction2()
-	helperFunction3()
-	helperFunction4()
-	helperFunction5()
-	helperFunction6()
-	helperFunction7()
-	helperFunction8()
-	helperFunction9()
-	helperFunction10()
-	helperFunction11()
-	helperFunction12()
-	helperFunction13()
-	helperFunction14()
-	helperFunction15()
-	helperFunction16()
-	helperFunction17()
-	helperFunction18()
-	helperFunction19()
-	helperFunction20()
-	helperFunction21()
-	helperFunction22()
-	helperFunction23()
-	helperFunction24()
-	helperFunction25()
-	helperFunction26()
-	helperFunction27()
-	helperFunction28()
-	helperFunction29()
-	helperFunction30()
-	helperFunction31()
-	helperFunction32()
-	helperFunction33()
-	helperFunction34()
-	helperFunction35()
-	helperFunction36()
-	helperFunction37()
-	helperFunction38()
-	helperFunction39()
-	helperFunction40()
-	helperFunction41()
-	helperFunction42()
-	helperFunction43()
-	helperFunction44()
-	helperFunction45()
-	helperFunction46()
-	helperFunction47()
-	helperFunction48()
-	helperFunction49()
-	helperFunction50()
-	
-	fmt.Println("\nThanks for playing!")
+
+	for gameState.GameRunning {
+		fmt.Println("\nMain Menu:")
+		fmt.Println("1. Explore")
+		fmt.Println("2. Check Status")
+		fmt.Println("3. Check Inventory")
+		fmt.Println("4. Use Item")
+		fmt.Println("5. Quit")
+		fmt.Print("Choose an option: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "1":
+			Explore(gameState)
+			if gameState.Player.Health <= 0 {
+				gameState.GameRunning = false
+			}
+		case "2":
+			DisplayStatus(gameState.Player)
+		case "3":
+			DisplayInventory(gameState.Inventory)
+		case "4":
+			if len(gameState.Inventory) == 0 {
+				fmt.Println("No items to use.")
+				break
+			}
+			DisplayInventory(gameState.Inventory)
+			fmt.Print("Select an item to use (number): ")
+			itemInput, _ := reader.ReadString('\n')
+			itemInput = strings.TrimSpace(itemInput)
+			index, err := strconv.Atoi(itemInput)
+			if err != nil || index < 1 || index > len(gameState.Inventory) {
+				fmt.Println("Invalid selection.")
+				break
+			}
+			gameState.Inventory = UseItem(gameState.Inventory, gameState.Player, index-1)
+		case "5":
+			fmt.Println("Thanks for playing! Goodbye.")
+			gameState.GameRunning = false
+		default:
+			fmt.Println("Invalid option. Please choose 1-5.")
+		}
+	}
 }
